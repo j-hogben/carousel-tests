08bd907eff82d73efce8981c3570667a7d6dc259
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-08-21 15:29:08 +0100

    Javascript - Style nav buttons on scroll

--- carousel.js
+++ carousel.js
@@ -1,6 +1,8 @@
 const currentIndexDisplay = document.querySelector('#currentSlideIndexDisplay');
 const carousel = document.querySelector('.carousel');
 const slides = document.querySelectorAll('.carousel__slide');
+const carouselPrev = document.querySelector('.carousel__button--prev');
+const carouselNext = document.querySelector('.carousel__button--next');
 
 let currentIndex = 0;
 
@@ -9,9 +11,10 @@ const updateCurrentIndex = () => {
   const scrollLeft = carousel.scrollLeft;
   const slideGap = getCarouselGap();
   const slideWidth = slides[0].offsetWidth + slideGap;
-  const newIndex = Math.floor(scrollLeft / slideWidth);
+  const newIndex = Math.round(scrollLeft / slideWidth);
   if (newIndex !== currentIndex) {
     currentIndex = newIndex;
+    styleInvalidCarouselNavBtns(currentIndex);
   }
   currentIndexDisplay.textContent = newIndex;
 };
@@ -19,5 +22,24 @@ const updateCurrentIndex = () => {
 // FUNCTION TO GET THE --carousel-gap VALUE
 const getCarouselGap = () =>
   parseFloat(getComputedStyle(carousel).getPropertyValue('--carousel-gap'));
+
+// FUNCTION TO ADD INVALID BUTTON STYLE DEPENDING ON CURRENT INDEX
+const styleInvalidCarouselNavBtns = (index) => {
+  /* IF INDEX IS 0 (FIRST SLIDE SELECTED), INVALIDATE THE
+  'PREVIOUS' CAROUSEL NAVIGATION BUTTON */
+  if (index === 0) {
+    carouselPrev.classList.add('carousel-button-invalid');
+  } else {
+    carouselPrev.classList.remove('carousel-button-invalid');
+  }
+
+  /* IF INDEX IS LAST SLIDE, INVALIDATE THE 'NEXT' 
+  CAROUSEL NAVIGATION BUTTON */
+  if (index === slides.length - 1) {
+    carouselNext.classList.add('carousel-button-invalid');
+  } else {
+    carouselNext.classList.remove('carousel-button-invalid');
+  }
+};
 
 carousel.addEventListener('scroll', updateCurrentIndex);
